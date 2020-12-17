e38e7b518b3efa0dbe8fa6f15ae370c04a94a7ca
Mary <korikokoro@mail.ru>
2020-12-17 17:53:29 +0300

    Add Routes in App

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 function App() {
   return (
-    <>
-      iWish
-    </>
-  );
+    <BrowserRouter>
+    <Switch>
+
+      <Route exact path='/'>
+        IWish 
+        Приложение по подбору подарков
+      </Route>
+
+      <Route path='/ideas/:category'>
+        тут идеи подарков
+      </Route>
+    
+      <Route path='/dashboard/:id'>
+        Личный кабинет
+      </Route>
+      
+    </Switch>
+  </BrowserRouter>
+  )
 }
 
 export default App;
