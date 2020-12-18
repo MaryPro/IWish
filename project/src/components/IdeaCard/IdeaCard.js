1c7a273d4c63455765c8f7242d6563df38ede45b
Mary <korikokoro@mail.ru>
2020-12-18 14:36:11 +0300

    add dropDown to ideaCard

--- project/src/components/IdeaCard/IdeaCard.js
+++ project/src/components/IdeaCard/IdeaCard.js
@@ -1,11 +1,24 @@
-import { Col } from 'react-bootstrap'
+import { Dropdown, DropdownButton, Col } from 'react-bootstrap'
 import style from './IdeaCard.module.css'
-export default function IdeaCard({idea}) {
+
+export default function IdeaCard({ idea }) {
   return (
     <div className={style.card}>
       <Col>
-        <div> <a href="#" > like </a> </div>
-        <img src={idea.img} alt={idea.titleGift} width="100"/>
+        <DropdownButton
+          className={style.dropdownToggle}
+          menuAlign="right"
+          title={<i class="far fa-heart"></i>}
+          id="dropdown-menu-align-right"
+        >
+          <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+          <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+          <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+        </DropdownButton>
+
+        <img src={idea.img} alt={idea.titleGift} width="100" />
         <div>
           {idea.rate}
           {idea.titleGift}
