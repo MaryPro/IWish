d5046308548dd43f52ff6510985ab08525ea10fe
Mary <korikokoro@mail.ru>
2020-12-18 12:48:04 +0300

    Add bootsrap card to categoryCard

--- project/src/components/CategoryCard/CategoryCard.js
+++ project/src/components/CategoryCard/CategoryCard.js
@@ -1,15 +1,21 @@
-import { Col } from 'react-bootstrap'
+import { Col, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-export default function CategoryCard({categoryObj}) {
-  const categoryRoute =  Object.keys(categoryObj)[0]
+export default function CategoryCard({ categoryObj }) {
+  const categoryRoute = Object.keys(categoryObj)[0]
   const category = categoryObj[categoryRoute]
   return (
     <div>
       <Col>
-       <Link to = {'/ideas/' + categoryRoute}>
-          <div>
-            {category}
-          </div>
+        <Link to={'/ideas/' + categoryRoute}>
+          <Card  
+            bg="success"
+            style={{ width: '10rem' , height: '10rem', color: 'white', marginBottom: '1rem'}}
+          >
+            <Card.Img  src={categoryObj.img} />
+            <Card.ImgOverlay>
+                {category}
+            </Card.ImgOverlay>
+          </Card>
         </Link>
       </Col>
     </div>
--- project/src/components/CategoryList/CategoryList.js
+++ project/src/components/CategoryList/CategoryList.js
@@ -4,12 +4,18 @@ import CategoryCard from '../CategoryCard/CategoryCard'
 export default function CategoryList() {
   
   const categories = [
-    {"loved" : "Любимому"},
-    {"family" : "Семье"},
-    {"friends" : "Друзьям"},
-    {"colleges" : "Коллегам"},
-    {"me" : "Себе"},
-    {"children" : "Детям"}
+    {"loved" : "Любимому",
+      img: "https://www.brighterkind.com/sites/default/files/Woodbury%20House-Gingerbread%20image.jpg"},
+    {"family" : "Семье",
+      img: "https://www.pulainfo.hr/wp/wp-content/uploads/2020/10/christmas.jpg"},
+    {"friends" : "Друзьям",
+      img: "https://www.history.com/.image/c_fill%2Ccs_srgb%2Cfl_progressive%2Ch_400%2Cq_auto:good%2Cw_620/MTY4ODE4ODA4MzY1MDAwNDY1/christmas-gettyimages-184652817.jpg"},
+    {"colleges" : "Коллегам",
+      img: "https://www.zebcare.nl/wp-content/uploads/2019/12/Christmas-1024x683.jpg"},
+    {"me" : "Себе",
+      img: "https://api.time.com/wp-content/uploads/2015/12/presents1.jpg?w=600&quality=85"},
+    {"children" : "Детям",
+      img: "https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/7ucwd-TWSE6PQ8ZPH-Full-Image_GalleryBackground-en-US-1583342948536._RI_.jpg"}
   ]
 
   return (
