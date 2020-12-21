40d376cff7d59920f14f05056727ae107544803c
Mary <korikokoro@mail.ru>
2020-12-21 13:08:38 +0300

    Add styles for ideaCard

--- project/src/components/IdeaCard/Star/Star.js
+++ project/src/components/IdeaCard/Star/Star.js
@@ -3,7 +3,7 @@ import style from '../IdeaCard.module.css'
 export default function Star({idea}) {
   return (
     <>
-      <div className={style.star} style={{fontSize: '40px', color: 'Dodgerblue'}}>
+      <div className={style.star} style={{fontSize: '40px', color: '#D6AD5F'}}>
             <i class="fas fa-star"></i>
       </div>
       <div  className={style.rate}>{idea.rate}</div> 
