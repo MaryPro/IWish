6cf5c03564fb5636ca4bc8ce1fce0fe19a743ead
KonstantinChebotarev <konstantin.spb@yandex.ru>
2020-12-24 10:57:49 +0300

    alert in reg & log

--- project/src/components/LoginModal/LoginModal.js
+++ project/src/components/LoginModal/LoginModal.js
@@ -14,7 +14,7 @@ function LoginModal({ show, setShow }) {
 
   const resultLogin = useSelector(store => store.user)
   const dispatch = useDispatch();
-  const [state, setState] = useState('')
+  const [state, setState] = useState(null)
 
   const subLog = (e) => {
     e.preventDefault();
@@ -44,7 +44,9 @@ function LoginModal({ show, setShow }) {
 
         <Modal.Header closeButton>
 
-        <Modal.Title>{state}</Modal.Title>
+        <Modal.Title>
+          {state && <div class="alert alert-danger" role="alert">{state}</div>}
+        </Modal.Title>
              </Modal.Header>
         <Modal.Body>
 
