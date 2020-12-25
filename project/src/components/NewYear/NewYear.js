c79e555e7dbadd68f75640d79d48f5cb6074f8ba
KonstantinChebotarev <konstantin.spb@yandex.ru>
2020-12-25 11:42:31 +0300

    button

--- project/src/components/NewYear/NewYear.js
+++ project/src/components/NewYear/NewYear.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import style from './NewYear.module.css'
 
-export default function NewYear() {
-  const [show, setShow] = useState(false)
+export default function NewYear({show, setShow}) {
+  // const [show, setShow] = useState(false)
 
   const handleShow = () => setShow(!show);
   let script
@@ -25,10 +25,10 @@ if(show){
          <div style={{height: '30px', background: 'url(https://uguide.ru/js/script/girlianda_uguide_ru_2.gif) repeat-x 100%'}}></div>
        </div> : console.log(':(')
   
-    }
-        <button className={style.btn} onClick={handleShow} >
-        <img  className={style.d} src="https://res.cloudinary.com/di5lpqwcp/image/upload/v1608839386/img/e7vkd6sbdkov08gpghgf.png" width='300px' alt=""/>
-        </button>
+    } <div className={style.btn} onClick={handleShow} >
+        <img   className={style.d} src="https://res.cloudinary.com/di5lpqwcp/image/upload/v1608839386/img/e7vkd6sbdkov08gpghgf.png" width='300px' alt=""/>
+        </div>
+       
     </>
   )
 }
